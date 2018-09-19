4069223e0ba75f2a2d568b113240decc34890a33
Francisco Santos <yoshi.df@gmail.com>
2018-09-19 02:54:11 -0300

    style: change docz plaground theme color

--- doczrc.js
+++ doczrc.js
@@ -3,5 +3,16 @@ export default {
   title: 'React awesome styled grid',
   description: 'A grid system layout for React using styled-components',
   port: 4000,
-  protocol: 'http'
+  protocol: 'http',
+  htmlContext: {
+    head: {
+      links: [{
+        rel: 'stylesheet',
+        href: 'https://codemirror.net/theme/dracula.css'
+      }]
+    }
+  },
+  themeConfig: {
+    codemirrorTheme: 'dracula'
+  }
 }
