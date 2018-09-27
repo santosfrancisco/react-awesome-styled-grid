62c6b0f7cbbca6248e51fe8e97af8151d9eb9ad2
Francisco Santos <yoshi.df@gmail.com>
2018-09-27 00:34:06 -0300

    feat: creates the Visible component

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import Container from './components/grid/container'
 import Row from './components/grid/row'
 import Col from './components/grid/col'
+import Visible from './components/util/visible'
 
-export { Container, Row, Col }
+export { Container, Row, Col, Visible }
