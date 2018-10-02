0616ddac17cfe85fdc32e3593eac4c3330d8efd6
Camila Belo <camilaibs@gmail.com>
2018-10-02 14:20:19 -0300

    test(visible): refactor tests that was verifying implementation

--- src/components/__tests__/visible.test.js
+++ src/components/__tests__/visible.test.js
@@ -4,64 +4,52 @@ import { BREAKPOINTS, DIMENSIONS } from '../../config'
 import Visible from '../util/visible'
 
 describe('<Visible />', () => {
+  const resize = width => {
+    global.window.innerWidth = width
+    global.window.dispatchEvent(new Event('resize'))
+  }
+
   it('should render default style correctly', () => {
     const tree = renderer.create(<Visible />).toJSON()
     expect(tree).toMatchSnapshot()
   })
 
   it('should initialize state', () => {
-    const instance = renderer.create(<Visible />).getInstance()
-    expect(instance.state).toEqual({ screen: 'lg' })
-  })
-
-  it('should listen resize and orientation events', () => {
-    const addEventListener = jest.fn()
-    global.window.addEventListener = addEventListener
-
-    const instance = renderer.create(<Visible />).getInstance()
-
-    expect(addEventListener.mock.calls[3]).toEqual(['orientationchange', instance.setScreen, false])
-    expect(addEventListener.mock.calls[4]).toEqual(['resize', instance.setScreen, false])
+    resize(1)
+    const root = renderer.create(<Visible xs >visible</Visible>).root
+    expect(root.children).toContain('visible')
+    resize(576)
+    expect(root.children).toHaveLength(0)
   })
 
   it('should unlisten resize and orientation events', () => {
-    const removeEventListener = jest.fn()
-    global.window.removeEventListener = removeEventListener
+    const spy = jest.spyOn(global.window, 'removeEventListener')
 
     const component = renderer.create(<Visible />)
     const instance = component.getInstance()
-
     component.unmount()
 
-    expect(removeEventListener.mock.calls[4]).toEqual(['orientationchange', instance.setScreen])
-    expect(removeEventListener.mock.calls[5]).toEqual(['resize', instance.setScreen])
+    expect(spy).toHaveBeenCalledWith('orientationchange', instance.setScreen)
+    expect(spy).toHaveBeenCalledWith('resize', instance.setScreen)
+
+    spy.mockRestore()
   })
 
   it('should control screen state', () => {
-    global.window.innerWidth = 576
-    const instance = renderer.create(<Visible />).getInstance()
-    instance.setScreen()
-    expect(instance.state.screen).toBe('sm')
+    resize(1)
+    const root = renderer.create(<Visible sm>visible</Visible>).root
+    expect(root.children).toHaveLength(0)
+
+    resize(576)
+    expect(root.children).toContain('visible')
   })
 
   it('should always be visible', () => {
-    const resize = width => {
-      global.window.innerWidth = width
-      global.window.dispatchEvent(new Event('resize'))
-    }
+    const root = renderer.create(<Visible xs sm md xl lg>visible</Visible>).root
 
-    const instance = renderer.create(<Visible xs sm md xl lg />).getInstance()
-    
     BREAKPOINTS.forEach(b => {
       resize(b)
-      expect(instance.isVisible()).toBe(true)
+      expect(root.children).toContain('visible')
     })
-  })
-
-  it('should be null if not screen on state', () => {
-    const instance = renderer.create(<Visible xs sm md xl lg />).getInstance()
-    
-    instance.state = {}
-    expect(instance.isVisible()).toBe(false)
   })
 })
