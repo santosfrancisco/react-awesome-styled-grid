1177128af386a430885331683b9a11b321705dcc
Francisco Farias <yoshi.df@gmail.com>
2018-10-27 17:12:39 -0300

    docs: update docz config

--- doczrc.js
+++ doczrc.js
@@ -4,17 +4,18 @@ export default {
   description: 'A grid system layout for React using styled-components',
   port: 4000,
   protocol: 'http',
+  indexHtml: 'public/index.html',
   htmlContext: {
     head: {
       links: [{
         rel: 'stylesheet',
         href: 'https://codemirror.net/theme/dracula.css'
       }]
-    }
+    },
+    favicon: 'public/favicon.ico'
   },
   themeConfig: {
     codemirrorTheme: 'dracula'
   },
-  dest: 'docs',
   propsParse: false
 }
