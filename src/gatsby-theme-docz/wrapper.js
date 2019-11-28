166484dddb48da0c056ebf0c83d553349db9efbd
Francisco Santos <fwfs1985@gmail.com>
2019-11-28 02:10:00 -0300

    feat: stylizes code tag on docz theme

--- src/gatsby-theme-docz/wrapper.js
+++ src/gatsby-theme-docz/wrapper.js
@@ -1,7 +1,9 @@
 import * as React from 'react'
 import { Helmet } from 'react-helmet-async'
+import styled from 'styled-components'
+import PropTypes from 'prop-types'
 
-const Wrapper = ({ children }) => <React.Fragment>
+const Wrapper = ({ className, children }) => <div className={className}>
   <Helmet>
     <meta charSet='utf-8' />
     <link rel='icon'
@@ -10,6 +12,19 @@ const Wrapper = ({ children }) => <React.Fragment>
     />
   </Helmet>
   {children}
-</React.Fragment>
+</div>
 
-export default Wrapper
+export default styled(Wrapper)`
+  code {
+    color: indianred;
+    margin: 0px 3px;
+    padding: 3px 5px;
+    border-radius: 3px;
+    background: rgb(239, 242, 247);
+  }
+`
+
+Wrapper.propTypes = {
+  className: PropTypes.string.isRequired,
+  children: PropTypes.node.isRequired
+}
