10c4efe4fa0868e15aa997aedae4138c9bfc9836
Francisco Santos <fwfs1985@gmail.com>
2020-05-07 19:14:29 -0300

    Converts the Hidden and Visible components to a functional component (#102)
    * include @use-it/event-listener@0.1.3
    
    * converts the Hidden component to a functional component
    
    * converts the Visible component to a functional component
    
    * fix type definitions

--- src/components/__tests__/hidden.test.js
+++ src/components/__tests__/hidden.test.js
@@ -1,7 +1,7 @@
 import React from 'react'
-import renderer from 'react-test-renderer'
+import renderer, { act } from 'react-test-renderer'
 import config from '../../config'
-import {Hidden} from '../util/hidden'
+import { Hidden } from '../util/hidden'
 
 describe('<Hidden />', () => {
   const resize = width => {
@@ -15,39 +15,35 @@ describe('<Hidden />', () => {
   })
 
   it('should initialize state', () => {
-    resize(1)
+    act(() => {
+      resize(1)
+    })
     const root = renderer.create(<Hidden xs>hidden</Hidden>).root
     expect(root.children).toHaveLength(0)
-    resize(768)
+    act(() => {
+      resize(768)
+    })
     expect(root.children).toContain('hidden')
-  })
-
-  it('should unlisten resize and orientation events', () => {
-    const spy = jest.spyOn(global.window, 'removeEventListener')
-
-    const component = renderer.create(<Hidden />)
-    const instance = component.getInstance()
-    component.unmount()
-
-    expect(spy).toHaveBeenCalledWith('orientationchange', instance.setScreen)
-    expect(spy).toHaveBeenCalledWith('resize', instance.setScreen)
-
-    spy.mockRestore()
   })
 
   it('should control screen state', () => {
-    resize(1)
+    act(() => {
+      resize(1)
+    })
     const root = renderer.create(<Hidden sm>hidden</Hidden>).root
     expect(root.children).toContain('hidden')
-    
-    resize(768)
+    act(() => {
+      resize(768)
+    })
     expect(root.children).toHaveLength(0)
   })
 
   it('should always be hidden', () => {
     const root = renderer.create(<Hidden xs sm md lg xl >hidden</Hidden>).root
     Object.values(config().breakpoints).forEach(b => {
-      resize(b * 16)
+      act(() => {
+        resize(b * 16)
+      })
       expect(root.children).toHaveLength(0)
     })
   })
--- src/components/__tests__/visible.test.js
+++ src/components/__tests__/visible.test.js
@@ -1,7 +1,7 @@
 import React from 'react'
-import renderer from 'react-test-renderer'
+import renderer, { act } from 'react-test-renderer'
 import config from '../../config'
-import {Visible} from '../util/visible'
+import { Visible } from '../util/visible'
 
 describe('<Visible />', () => {
   const resize = width => {
@@ -15,39 +15,35 @@ describe('<Visible />', () => {
   })
 
   it('should initialize state', () => {
-    resize(1)
+    act(() => {
+      resize(1)
+    })
     const root = renderer.create(<Visible xs>visible</Visible>).root
     expect(root.children).toContain('visible')
-    resize(768)
+    act(() => {
+      resize(768)
+    })
     expect(root.children).toHaveLength(0)
-  })
-
-  it('should unlisten resize and orientation events', () => {
-    const spy = jest.spyOn(global.window, 'removeEventListener')
-
-    const component = renderer.create(<Visible />)
-    const instance = component.getInstance()
-    component.unmount()
-
-    expect(spy).toHaveBeenCalledWith('orientationchange', instance.setScreen)
-    expect(spy).toHaveBeenCalledWith('resize', instance.setScreen)
-
-    spy.mockRestore()
   })
 
   it('should control screen state', () => {
-    resize(1)
+    act(() => {
+      resize(1)
+    })
     const root = renderer.create(<Visible sm>visible</Visible>).root
     expect(root.children).toHaveLength(0)
-
-    resize(768)
+    act(() => {
+      resize(768)
+    })
     expect(root.children).toContain('visible')
   })
 
   it('should always be visible', () => {
     const root = renderer.create(<Visible xs sm md xl lg>visible</Visible>).root
     Object.values(config().breakpoints).forEach(b => {
-      resize(b * 16)
+      act(() => {
+        resize(b * 16)
+      })
       expect(root.children).toContain('visible')
     })
   })
