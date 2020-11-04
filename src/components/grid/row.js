d2b812e05ee5ce7aebea74df8d5d0148dccb6795
Paulo Ricardo Xavier Giusti <prxg22@gmail.com>
2020-11-04 19:54:35 -0300

    fix: reverse wrap direction when using row`s reverse props (#126)
    * fix: reverse wrap direction when using row`s reverse props
    
    * fix: apply flex-wrap in requested dimensions when revert is an array
    
    * fix: add semicolon
    
    * test: test if reverse is appllyed correctly on requested dimensions
    
    * fix(Row): force wrap behavior in dimensions in which reverse isn`t requested

--- src/components/grid/row.js
+++ src/components/grid/row.js
@@ -1,4 +1,3 @@
-
 import styled, { css } from 'styled-components'
 import PropTypes from 'prop-types'
 import config, { DIMENSIONS } from '../../config'
@@ -10,58 +9,89 @@ const Row = styled.div`
   flex-wrap: wrap;
   
   ${p => css`
-    ${DIMENSIONS.map(d =>
-    config(p).container[ d ] && config(p).media[ d ]`
+    ${DIMENSIONS.map(
+      d =>
+        config(p).container[d] &&
+        config(p).media[d]`
       margin-left: -${config(p).gutterWidth[d] / 2}rem;
       margin-right: -${config(p).gutterWidth[d] / 2}rem;
-    `)}
+    `,
+    )}
   `}
 
-  ${p => p.reverse && css`
-    ${Array.isArray(p.reverse)
-    ? DIMENSIONS.map(d =>
-      config(p).breakpoints[d] && config(p).media[d]`
-        flex-direction: ${p.reverse.indexOf(d) !== -1 ? `row-reverse` : `row`};
-      `)
-    : 'flex-direction: row-reverse;'}
-  `}
+  ${p =>
+    p.reverse &&
+    css`
+      ${Array.isArray(p.reverse)
+        ? DIMENSIONS.map(
+            d =>
+              config(p).breakpoints[d] &&
+              config(p).media[d]`
+         ${
+           p.reverse.indexOf(d) !== -1
+             ? `
+             flex-direction: row-reverse;
+             flex-wrap: wrap-reverse;
+             `
+             : `
+                 flex-direction: row;
+                 flex-wrap: wrap;
+               `
+         };
+        
+      `,
+          )
+        : `
+            flex-direction: row-reverse;
+            flex-wrap: wrap-reverse;
+          `}
+    `}
 
-  ${p => p.align && css`
-    ${typeof p.align === 'object'
-    ? DIMENSIONS.map(d => config(p).breakpoints[d] && config(p).media[d]`${p.align[d] && `align-items: ${p.align[d]}`}`)
-    : `align-items: ${p.align};`}
-  `}
+  ${p =>
+    p.align &&
+    css`
+      ${typeof p.align === 'object'
+        ? DIMENSIONS.map(
+            d =>
+              config(p).breakpoints[d] &&
+              config(p).media[d]`${p.align[d] && `align-items: ${p.align[d]}`}`,
+          )
+        : `align-items: ${p.align};`}
+    `}
   
-  ${p => p.justify && css`
-    ${typeof p.justify === 'object'
-    ? DIMENSIONS.map(d => config(p).breakpoints[d] && config(p).media[d]`${p.justify[d] && `justify-content: ${p.justify[d]}`}`)
-    : `justify-content: ${p.justify};`}
-  `}
+  ${p =>
+    p.justify &&
+    css`
+      ${typeof p.justify === 'object'
+        ? DIMENSIONS.map(
+            d =>
+              config(p).breakpoints[d] &&
+              config(p).media[d]`${p.justify[d] &&
+                `justify-content: ${p.justify[d]}`}`,
+          )
+        : `justify-content: ${p.justify};`}
+    `}
 
-  ${({ debug }) => debug && css`
-    background-color: #5901ad40;
-    outline: #fff solid 1px;
-  `}
+  ${({ debug }) =>
+    debug &&
+    css`
+      background-color: #5901ad40;
+      outline: #fff solid 1px;
+    `}
 `
 
 Row.displayName = 'Row'
 
-const boolOrArray = PropTypes.oneOfType([
-  PropTypes.bool,
-  PropTypes.array
-])
+const boolOrArray = PropTypes.oneOfType([PropTypes.bool, PropTypes.array])
 
-const stringOrObject = PropTypes.oneOfType([
-  PropTypes.string,
-  PropTypes.object
-])
+const stringOrObject = PropTypes.oneOfType([PropTypes.string, PropTypes.object])
 
 Row.propTypes = {
   reverse: boolOrArray,
   align: stringOrObject,
   justify: stringOrObject,
   children: PropTypes.node,
-  debug: PropTypes.bool
+  debug: PropTypes.bool,
 }
 
 export default Row
