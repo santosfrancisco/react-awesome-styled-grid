8128efbe700ad83d64947be9ac78776612c072bd
Aleksei <djaleksmart@gmail.com>
2021-01-26 15:38:13 +0200

    add order

--- src/components/__tests__/col.test.js
+++ src/components/__tests__/col.test.js
@@ -1,189 +1,194 @@
-import React from "react";
-import renderer from "react-test-renderer";
-import { DIMENSIONS, BASE_CONF } from "../../config";
-import Col from "../grid/col";
-import "jest-styled-components";
+import React from 'react'
+import renderer from 'react-test-renderer'
+import { DIMENSIONS, BASE_CONF } from '../../config'
+import Col from '../grid/col'
+import 'jest-styled-components'
 
-describe("<Col />", () => {
-  it("should render default style correctly", () => {
-    const tree = renderer.create(<Col />).toJSON();
-    expect(tree).toMatchSnapshot();
-  });
+describe('<Col />', () => {
+  it('should render default style correctly', () => {
+    const tree = renderer.create(<Col />).toJSON()
+    expect(tree).toMatchSnapshot()
+  })
 
-  it("should have no gutter", () => {
-    const tree = renderer.create(<Col noGutter />).toJSON();
+  it('should have no gutter', () => {
+    const tree = renderer.create(<Col noGutter />).toJSON()
 
-    expect(tree).toHaveStyleRule("padding-right", undefined);
-    expect(tree).toHaveStyleRule("padding-left", undefined);
-  });
+    expect(tree).toHaveStyleRule('padding-right', undefined)
+    expect(tree).toHaveStyleRule('padding-left', undefined)
+  })
 
-  it("should have default gutter", () => {
-    const tree = renderer.create(<Col />).toJSON();
+  it('should have default gutter', () => {
+    const tree = renderer.create(<Col />).toJSON()
 
-    const { gutterWidth, breakpoints } = BASE_CONF;
+    const { gutterWidth, breakpoints } = BASE_CONF
 
-    DIMENSIONS.forEach((d) => {
+    DIMENSIONS.forEach(d => {
       const media = {
-        media: `only screen and (min-width: ${breakpoints[d]}rem)`,
-      };
+        media: `only screen and (min-width: ${breakpoints[d]}rem)`
+      }
 
-      const defaultGutter = `${gutterWidth[d] / 2}rem`;
-      expect(tree).toHaveStyleRule("padding-right", defaultGutter, media);
-      expect(tree).toHaveStyleRule("padding-left", defaultGutter, media);
-    });
-  });
+      const defaultGutter = `${gutterWidth[d] / 2}rem`
+      expect(tree).toHaveStyleRule('padding-right', defaultGutter, media)
+      expect(tree).toHaveStyleRule('padding-left', defaultGutter, media)
+    })
+  })
 
-  it("should have the specified size", () => {
+  it('should have the specified size', () => {
     const props = {
       xs: 1,
       sm: 2,
       md: 3,
       lg: 4,
-      xl: 5,
-    };
+      xl: 5
+    }
 
-    const tree = renderer.create(<Col {...props} />).toJSON();
+    const tree = renderer.create(<Col {...props} />).toJSON()
 
-    const { breakpoints, columns } = BASE_CONF;
+    const { breakpoints, columns } = BASE_CONF
 
-    DIMENSIONS.forEach((d) => {
-      const propotionalSize = (props[d] / columns[d]) * 100;
+    DIMENSIONS.forEach(d => {
+      const propotionalSize = props[d] / columns[d] * 100
 
       const media = {
-        media: `only screen and (min-width: ${breakpoints[d]}rem)`,
-      };
+        media: `only screen and (min-width: ${breakpoints[d]}rem)`
+      }
 
-      expect(tree).toHaveStyleRule("flex", `1 1 ${propotionalSize}%`, media);
-      expect(tree).toHaveStyleRule("max-width", `${propotionalSize}%`, media);
-    });
-  });
+      expect(tree).toHaveStyleRule('flex', `1 1 ${propotionalSize}%`, media)
+      expect(tree).toHaveStyleRule('max-width', `${propotionalSize}%`, media)
+    })
+  })
 
-  it("should have a offset space for each media", () => {
+  it('should have a offset space for each media', () => {
     const props = {
       offset: {
         xs: 1,
         sm: 2,
         md: 3,
         lg: 4,
-        xl: 5,
-      },
-    };
+        xl: 5
+      }
+    }
 
-    const tree = renderer.create(<Col {...props} />).toJSON();
+    const tree = renderer.create(<Col {...props} />).toJSON()
 
-    const { breakpoints, columns } = BASE_CONF;
+    const { breakpoints, columns } = BASE_CONF
 
-    DIMENSIONS.forEach((d) => {
+    DIMENSIONS.forEach(d => {
       expect(tree).toHaveStyleRule(
-        "margin-left",
-        `${(props.offset[d] / columns[d]) * 100}%`,
-        {
-          media: `only screen and (min-width: ${breakpoints[d]}rem)`,
+        'margin-left', `${props.offset[d] / columns[d] * 100}%`, {
+          media: `only screen and (min-width: ${breakpoints[d]}rem)`
         }
-      );
-    });
-  });
+      )
+    })
+  })
 
-  it.only("should have a order for each media", () => {
+  it('should have a order space for each media', () => {
     const props = {
       order: {
         xs: 5,
         sm: 3,
         md: 2,
         lg: 6,
-        xl: 8,
-      },
-    };
+        xl: 8
+      }
+    }
 
-    const tree = renderer.create(<Col {...props} />).toJSON();
+    const tree = renderer.create(<Col {...props} />).toJSON()
 
-    const { breakpoints } = BASE_CONF;
+    const { breakpoints } = BASE_CONF
 
-    DIMENSIONS.forEach((d) => {
-      expect(tree).toHaveStyleRule("order", `${props.order[d]}`, {
-        media: `only screen and (min-width: ${breakpoints[d]}rem)`,
-      });
-    });
-  });
+    DIMENSIONS.forEach(d => {
+      expect(tree).toHaveStyleRule(
+        'order', `${props.order[d]}`, {
+          media: `only screen and (min-width: ${breakpoints[d]}rem)`
+        }
+      )
+    })
+  })
 
-  it("should have a reverse direction for each media", () => {
-    const tree = renderer.create(<Col reverse />).toJSON();
+  it('should have a reverse direction for each media', () => {
+    const tree = renderer.create(<Col reverse />).toJSON()
 
-    expect(tree).toHaveStyleRule("flex-direction", `column-reverse`);
-  });
+    expect(tree).toHaveStyleRule(
+      'flex-direction', `column-reverse`)
+  })
 
-  it("should have the css rule justify-content center", () => {
+  it('should have the css rule justify-content center', () => {
     const props = {
-      justify: "center",
-    };
-    const tree = renderer.create(<Col {...props} />).toJSON();
+      justify: 'center'
+    }
+    const tree = renderer.create(<Col {...props} />).toJSON()
 
-    expect(tree).toHaveStyleRule("justify-content", "center");
-  });
+    expect(tree).toHaveStyleRule('justify-content', 'center')
+  })
 
-  it("should have the css rule justify-content center only in MD screen", () => {
+  it('should have the css rule justify-content center only in MD screen', () => {
     const props = {
       justify: {
-        md: "center",
-      },
-    };
+        md: 'center'
+      }
+    }
 
-    const tree = renderer.create(<Col {...props} />).toJSON();
+    const tree = renderer.create(<Col {...props} />).toJSON()
 
-    const { breakpoints } = BASE_CONF;
+    const { breakpoints } = BASE_CONF
 
-    DIMENSIONS.forEach((d) => {
-      expect(tree).toHaveStyleRule("justify-content", props.justify[d], {
-        media: `only screen and (min-width: ${breakpoints[d]}rem)`,
-      });
-    });
-  });
+    DIMENSIONS.forEach(d => {
+      expect(tree).toHaveStyleRule(
+        'justify-content', props.justify[d], {
+          media: `only screen and (min-width: ${breakpoints[d]}rem)`
+        }
+      )
+    })
+  })
 
-  it("should have the css rule align-items center", () => {
+  it('should have the css rule align-items center', () => {
     const props = {
-      align: "center",
-    };
-    const tree = renderer.create(<Col {...props} />).toJSON();
+      align: 'center'
+    }
+    const tree = renderer.create(<Col {...props} />).toJSON()
 
-    expect(tree).toHaveStyleRule("align-items", "center");
-  });
+    expect(tree).toHaveStyleRule('align-items', 'center')
+  })
 
-  it("should have the css rule align-items center only in MD screen", () => {
+  it('should have the css rule align-items center only in MD screen', () => {
     const props = {
       align: {
-        md: "center",
-      },
-    };
+        md: 'center'
+      }
+    }
 
-    const tree = renderer.create(<Col {...props} />).toJSON();
+    const tree = renderer.create(<Col {...props} />).toJSON()
 
-    const { breakpoints } = BASE_CONF;
+    const { breakpoints } = BASE_CONF
 
-    DIMENSIONS.forEach((d) => {
-      expect(tree).toHaveStyleRule("align-items", props.align[d], {
-        media: `only screen and (min-width: ${breakpoints[d]}rem)`,
-      });
-    });
-  });
+    DIMENSIONS.forEach(d => {
+      expect(tree).toHaveStyleRule(
+        'align-items', props.align[d], {
+          media: `only screen and (min-width: ${breakpoints[d]}rem)`
+        }
+      )
+    })
+  })
 
-  it("should have a reverse direction for each media", () => {
-    const tree = renderer.create(<Col reverse={["md"]} />).toJSON();
-    const { breakpoints } = BASE_CONF;
-    expect(tree).toHaveStyleRule("flex-direction", "column-reverse", {
-      media: `only screen and (min-width: ${breakpoints["md"]}rem)`,
-    });
-  });
+  it('should have a reverse direction for each media', () => {
+    const tree = renderer.create(<Col reverse={['md']} />).toJSON()
+    const { breakpoints } = BASE_CONF
+    expect(tree).toHaveStyleRule('flex-direction', 'column-reverse', {
+      media: `only screen and (min-width: ${breakpoints['md']}rem)`
+    })
+  })
 
-  it("should have different style when it`s debug props is true", () => {
-    const tree = renderer.create(<Col debug />).toJSON();
-    expect(tree).toHaveStyleRule("background-color", "#5901ad40");
-    expect(tree).toHaveStyleRule("outline", "#fff solid 1px");
-  });
+  it('should have different style when it`s debug props is true', () => {
+    const tree = renderer.create(<Col debug />).toJSON()
+    expect(tree).toHaveStyleRule('background-color', '#5901ad40')
+    expect(tree).toHaveStyleRule('outline', '#fff solid 1px')
+  })
 
-  it("should pass every props", () => {
-    const tree = renderer.create(<Col id="uniqueID" lol="anotherprop" />);
-    const testInstance = tree.root;
-    expect(testInstance.findByType(Col).props.id).toBe("uniqueID");
-    expect(testInstance.findByType(Col).props.lol).toBe("anotherprop");
-  });
-});
+  it('should pass every props', () => {
+    const tree = renderer.create(<Col id='uniqueID' lol='anotherprop' />)
+    const testInstance = tree.root
+    expect(testInstance.findByType(Col).props.id).toBe('uniqueID')
+    expect(testInstance.findByType(Col).props.lol).toBe('anotherprop')
+  })
+})
