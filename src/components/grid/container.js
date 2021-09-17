7d046cc4995e945ea94a3cca6b512b870fcd0991
Francisco Santos <francisco.santos@midway.com.br>
2021-09-16 23:48:10 -0300

    fix: container max-width

--- src/components/grid/container.js
+++ src/components/grid/container.js
@@ -1,11 +1,7 @@
-import PropTypes from 'prop-types'
-import styled, {
-  css
-} from 'styled-components'
+import PropTypes from "prop-types";
+import styled, { css } from "styled-components";
 
-import config, {
-  DIMENSIONS
-} from '../../config'
+import config, { DIMENSIONS } from "../../config";
 
 const Container = styled.div`
   margin-right: auto;
@@ -13,34 +9,48 @@ const Container = styled.div`
   max-width: 100%;
   box-sizing: border-box;
 
-  ${p => css`
-    ${DIMENSIONS.map(d =>
-    config(p).container[ d ] && config(p).media[ d ]`
-      padding-left: ${config(p).paddingWidth[ d ]}rem;
-      padding-right: ${config(p).paddingWidth[ d ]}rem;
-    `)}
+  ${(p) => css`
+    ${DIMENSIONS.map(
+      (d) =>
+        config(p).container[d] &&
+        config(p).media[d]`
+      padding-left: ${config(p).paddingWidth[d]}rem;
+      padding-right: ${config(p).paddingWidth[d]}rem;
+    `
+    )}
   `}
   
 
-  ${p => !p.fluid && css`
-    ${DIMENSIONS.map(d =>
-    config(p).container[ d ] && config(p).media[ d ]`
-      ${typeof config(p).container[d] === 'number' ? `max-width: ${config(p).container[ d ]}rem;` : `max-width: 100%;`}
-    `)}
-  `}
+  ${(p) =>
+    !p.fluid &&
+    css`
+      ${DIMENSIONS.map(
+        (d) =>
+          config(p).container[d] &&
+          config(p).media[d]`
+      ${
+        typeof config(p).container[d] === "number"
+          ? `max-width: ${config(p).container[d]}rem;`
+          : `width: 100%;`
+      }
+    `
+      )}
+    `}
 
-  ${({ debug }) => debug && css`
-    background-color: #5901ad40;
-    outline: #fff solid 1px;
-  `}
-`
+  ${({ debug }) =>
+    debug &&
+    css`
+      background-color: #5901ad40;
+      outline: #fff solid 1px;
+    `}
+`;
 
-Container.displayName = 'Container'
+Container.displayName = "Container";
 
 Container.propTypes = {
   fluid: PropTypes.bool,
   children: PropTypes.node,
-  debug: PropTypes.bool
-}
+  debug: PropTypes.bool,
+};
 
-export default Container
+export default Container;
